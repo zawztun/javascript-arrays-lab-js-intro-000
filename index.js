9c7b2bc84cd5ccc88aba838ccfae1cd06ed01c79
Donal tun <americanarakan@gmail.com>
2019-08-19 04:43:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var kittens = ['MIlo', 'Otis', 'Garfield'] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
